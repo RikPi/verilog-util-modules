05ea60cc87d8e3557ab91081252b0587285e6275
Riccardo Testa <riccardotesta.pi@gmail.com>
2024-06-07 08:39:04 +0200

    Added MDIO master error state and pattern always cycle

--- communication/mdio/master/mdio_master.v
+++ communication/mdio/master/mdio_master.v
@@ -3,7 +3,7 @@ module mdio_master(
     input wire clk_2m5_neg,                         //! Negated 2.5MHz clock
     input wire clk_core,                            //! Clock used for the core, sync with 2.5MHz
     input wire aReset,                              //! Asynchronous reset
-    input wire mdio_en,                             //! MDIO enable signal, lifts clock gating
+    input wire i_mdio_en,                           //! MDIO enable signal, lifts clock gating
     input wire i_op_write,                          //! MDIO write operation signal
     input wire i_op_read,                           //! MDIO read operation signal
     input wire i_op_addr,                           //! MDIO address operation signal
@@ -24,11 +24,12 @@ module mdio_master(
     // Parameter declaration
     localparam ST_IDLE = 3'h0;                      //! MDIO FSM stays idle when it is not enabled
     localparam ST_START = 3'h1;                     //! MDIO starts when enabled and lifts clock gating
-    localparam ST_SEND_HIGH_BITS = 3'h2;            //! MDIO sends the first 32 high bits to initiate the transaction
+    localparam ST_SEND_PREAMBLE = 3'h2;             //! MDIO sends the first 32 high bits preamble to initiate the transaction
     localparam ST_SEND_MESSAGE = 3'h3;              //! MDIO sends the pattern
     localparam ST_TURNAROUND_WAIT = 3'h4;           //! MDIO waits the turnaround from the slave
     localparam ST_READ_RESPONSE = 3'h5;             //! If operation is a read, MDIO waits for the response after the turnaround
     localparam ST_STOP = 3'h6;                      //! Transaction is finished, now clear variables and go back in idle
+    localparam ST_ERROR = 3'h7;                     //! Error state, triggered by wrong op passed
 
     // Reg declarations
     reg [15:0] data_out;                            //! If operation is read, this is the result in output
@@ -39,6 +40,8 @@ module mdio_master(
     reg [5:0] counter;                              //! Counter for MDIO bits (based on 2.5MHz clk)
     reg [5:0] counter_next;                         //! Next counter value
     reg counter_en;                                 //! Enable bit for MDIO bit counter
+    reg preamble_en;                                //! Signal to send the preamble
+    reg preamble_en_next;                           //! Next signal for preamble
 
     // Wire declarations
     wire [1:0] mdio_ST;                             //! ST field of MDIO message, indicates the clause. 01 for cl22, 00 for cl45
@@ -62,10 +65,14 @@ module mdio_master(
     wire cl_22_op_valid;                            //! Validity of CL22 operation
     wire cl45_op_valid;                             //! Validity of CL45 operation
     wire mdio_op_valid;                             //! Valid CL22 or CL45 operation
+    wire mdc;                                       //! MDC internal wire, connects to the 2.5MHz negated clk
 
 
     // Wire assignments
     assign o_data = data_out;
+    assign o_mdio = mdio;
+    assign o_mdc = mdc;
+    assign mdc = i_mdio_en ? clk_2m5_neg : 1'b0;
 
     assign mdio_ST = i_cl45 ? 2'b00 : 2'b01;        //! The ST field of MDIO is 01 for Clause 22 accesses and 00 for Clause 45
     assign mdio_OP = 
@@ -85,6 +92,14 @@ module mdio_master(
     //! The MDIO operation is valid if either cl45 or cl22 operations are valid
     assign mdio_op_valid = cl22_op_valid | cl45_op_valid;
 
+    always @(posedge clk_2m5_neg, posedge aReset) begin
+        if(aReset) begin
+            mdio_next <= 1'b0;
+
+        end
+        
+    end
+
     always @(posedge clk_core, posedge aReset) begin : fsm_seq //! Sequential part of the MDIO master FSM
         if(aReset) begin
             state <= ST_IDLE;
@@ -93,6 +108,7 @@ module mdio_master(
         else begin
             state <= state_next;
             mdio <= mdio_next;
+            preamble_en <= preamble_en_next;
             
         end
     end
@@ -104,11 +120,13 @@ module mdio_master(
             
         ST_START : begin
             mdio_next <= 1'b0;
-            state_next <= ST_SEND_HIGH_BITS;
+            state_next <= ST_SEND_PREAMBLE;
         end
 
-        ST_SEND_HIGH_BITS : begin
+        ST_SEND_PREAMBLE : begin
             mdio_next <= 1'b1;
+            preamble_en_next <= 1'b1;
+
         end
 
         endcase
